a0459b3fd044835ad6d56af8f1c0c7eb7e2a08aa
Dhruva Raju <49dhruva@gmail.com>
2021-08-23 17:31:59 +0530

    feat(inital): first version

---
+++ src/main/java/com/sahaya/sahayaservices/SahayaServicesApplication.java
@@ -0,0 +1,13 @@
+package com.sahaya.sahayaservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahayaServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SahayaServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sahaya/sahayaservices/SahayaServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sahaya.sahayaservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahayaServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
